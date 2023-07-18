c7c1cb88e538bfb580d5a7fdbd32667d78e51b8b
Adrian Coronel <adrianonel78@gmail.com>
2023-07-18 11:26:26 -0500

    Creación de login y logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -36,6 +36,44 @@ export const register = async (req, res) => {
 
 };
 
-export const login = (req, res) => {
-  res.send('login')
+export const login = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    // Buscamos si el user existe
+    const userFound = await User.findOne({email});
+
+    // En caso el usuario no se encuentre en DB
+    if (!userFound) return res.status(400).json({message: 'User not found'})
+
+    // Si existe, se compará contraseñas
+    const isMatch = await bcrypt.compare(password, userFound.password)
+
+    // Si la contraseña ingresada es incorrecta
+    if (!isMatch) return res.status(400).json({message: 'Invalid credentials'})
+
+    // Crea un token con el ID del usuario encontrado
+    const token = await createAccessToken({id: userFound._id})
+    res.cookie('token',token) // metodo de express para crear un cookie
+    res.json({
+      _id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt
+    })
+
+
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+
 };
+
+export const logout = (req, res) => {
+  // El token con el nombre "token" tendrá un valor vacio y expirá en un tiempo de 0
+  res.cookie('token', '',{
+    expires: new Date(0)
+  });
+  return res.sendStatus(200);
+}
